06a92673626aeca5a55b26bcf64b58963ca41026
gagaf7 <145220548+gagaf7@users.noreply.github.com>
2024-01-04 22:57:08 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // on récupère tous les élémenat nécessaires
 const audio = document.querySelector("audio");
-const track = document.querySelector("#track");
+const track = document.getElementById("track");
 const elapsed = document.querySelector("#elapsed");
 let trackTime = document.querySelector("#track-time");
 const playButtom = document.querySelector("#play-buttom");
@@ -19,41 +19,44 @@ const check = document.querySelector("#check");
 const chooseBoutonRight = document.getElementById("check.buttom-right");
 const chooseBoutonLeft = document.getElementById("check.buttom-left");
 
+
 //on récupère la durer du mp3
 let duration = audio.duration;
-const musique1 ={
-    source: "musique/Legends Never Die (ft. Against The Current)  Worlds 2017 - League of Legends.mp3",
-    clips : "Clips_video/Legends Never Die (ft. Against The Current)  Worlds 2017 - League of Legends.mp4",
-    temps : musique.duration,
-};
-const musique2 ={
-    source: "musique/Overlord III - Voracity (Opening)  ENGLISH Ver  AmaLee.mp3",
-    clips : "Clips_video/Overlord III - Voracity (Opening)  ENGLISH Ver  AmaLee.mp4",
-    temps : musique.duration,
-};
-const musique3 ={
-    source:"musique/Suzume no Tojimari OST - Main Theme Full[Suzume]by RADWIMPS ft. toaka.mp3"  ,
-    clips:"Clips_video/Suzume by Radwimps - Music Video『Suzume no Tojimari』.mp4",
-};
-const musique4 ={
-    source:"musique/Nyan Cat! [Official].mp3" ,
-    clips:"",
-};
 
 
+
+  const musique1 ={
+      source: "musique/Legends Never Die (ft. Against The Current)  Worlds 2017 - League of Legends.mp3",
+      clips : "Clips_video/Legends Never Die (ft. Against The Current)  Worlds 2017 - League of Legends.mp4",
+      temps : musique.duration,
+    };
+    const musique2 ={
+        source: "musique/Overlord III - Voracity (Opening)  ENGLISH Ver  AmaLee.mp3",
+        clips : "Clips_video/Overlord III - Voracity (Opening)  ENGLISH Ver  AmaLee.mp4",
+        temps : musique.duration,
+    };
+    const musique3 ={
+        source:"musique/Suzume no Tojimari OST - Main Theme Full[Suzume]by RADWIMPS ft. toaka.mp3"  ,
+        clips:"Clips_video/Suzume by Radwimps - Music Video『Suzume no Tojimari』.mp4",
+    };
+    const musique4 ={
+        source:"musique/Nyan Cat! [Official].mp3" ,
+        clips:"",
+    };
+
+
+//intialisation de la boucle 
 let i = 0
-let situe = 0
-if (i <= 0){
-    i = 0
-};
 const playlist =[musique1,musique2, musique3, musique4];
 
-
+//initialisation de la fin de la boucle
 const taille = playlist.length;
+//permet de modifier le texte pour le temps de la musique
 trackTime.textContent = buildDuration(duration);
 
 video.style.display ="none";
 
+//permet de choisir entre l'image ou la vidéo
 chooseBoutonRight.addEventListener("click", function(){
     check.textContent = "vidéo";
     video.style.display = "initial";
@@ -98,6 +101,7 @@ stopButtom.addEventListener("click", function(){
     this.style.display = "none";
 });
 
+//permet de gérer le passage de musique vers l'avant
 skipButtom.addEventListener("click", function(){
     audio.pause();
     audio.currentTime = 0;
@@ -105,17 +109,17 @@ skipButtom.addEventListener("click", function(){
     pauseButtom.style.display = "none";
     stopButtom.style.display ="none";
     skipStartButtom.style.display ="initial";
-    i = i + 1 ; 
-    if (i >= taille - 1){
+    i = i + 1 ;  //permet de gérer l'indexation des musiques pour aller sur une musiques en avant
+    if (i >= taille - 1){    //permet d'arêter la boucle en cachant le bouton d'avancer
         i = taille - 1;
         this.style.display = "none";
     };
-    console.log(i);
+    //modifie la source de la musique pour changer de musique
     audio.src= playlist[i].source;
     video.src = playlist[i].clips;
 });
 
-
+//permet de gérer le passage de musique vers l'arrière
 skipStartButtom.addEventListener("click", function(){
     audio.pause();
     audio.currentTime = 0;
@@ -123,11 +127,12 @@ skipStartButtom.addEventListener("click", function(){
     pauseButtom.style.display = "none";
     stopButtom.style.display ="none";
     skipButtom.style.display = "initial";
-    i = i -1;
-    if (i <= 0){
+    i = i -1;   //permet de gérer l'indexation des musiques pour aller sur une musiques en arrière
+    if (i <= 0){    //permet de ne pas passer en index négatif pour avoir les musiques
         i = 0;
         this.style.display="none";
     };
+    //modifie la source de la musiques + la vidéo
     audio.src= playlist[i].source;
     video.src = playlist[i].clips;
 });
